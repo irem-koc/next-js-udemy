e1b6dbea5e94a80da05c8446281dd4832b59da5e
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-10-15 20:58:43 +0300

    Refactor header component to use a responsive navigation bar

---
+++ src/components/hero.tsx
@@ -0,0 +1,22 @@
+import Image, { StaticImageData } from "next/image";
+
+type Props = {
+  imgAlt: string;
+  imgUrl: StaticImageData;
+  title: string;
+};
+
+const Hero = ({ imgAlt, imgUrl, title }: Props) => {
+  return (
+    <div className="relative h-screen">
+      <div className="absolute -z-10 inset-0">
+        <Image src={imgUrl} alt={imgAlt} fill style={{ objectFit: "cover" }} />
+      </div>
+      <div className="flex justify-between items-center pt-48">
+        <h1 className="text-white text-6xl">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
