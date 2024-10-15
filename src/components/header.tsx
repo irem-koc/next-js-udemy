4fe64bdd8268f34b49793fb300ad512c5eb7ca39
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-10-15 20:58:39 +0300

    Refactor header component to use a responsive navigation bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div>
-      <Link href="/">Cloud Corp</Link>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
+    <div className="w-full absolute text-white z-10 ">
+      <nav className="container relative flex justify-between items-center flex-wrap">
+        <div className="font-bold text-3xl">
+          <Link href="/">Cloud Corp</Link>
+        </div>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
+      </nav>
     </div>
   );
 }
